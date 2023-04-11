29fa5b11bd3ef894883ed83d7741fcc183384a70
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-04-11 21:16:54 +0600

    job details page error fix

--- src/components/SingleJobDeteils.jsx
+++ src/components/SingleJobDeteils.jsx
@@ -1,12 +1,92 @@
-import React from 'react';
+import React from "react";
+import "./SingleJobDeteils.css";
+import { CalendarDaysIcon, CurrencyDollarIcon, EnvelopeIcon, MapPinIcon, PhoneIcon } from "@heroicons/react/24/solid";
+import { Button } from "flowbite-react";
 
-const SingleJobDeteils = ({job}) => {
-  const { company_logo,title} = job; 
-  console.log(company_logo);
-  // const {id, company_logo, title,company_name, remote_or_onsite, location, fulltime_or_parttime, salary} = job; 
+const SingleJobDeteils = ({ job }) => {
+  // console.log(job);
+  const {
+    description,
+    responsibility,
+    salary,
+    educational_requirements,
+    experiences,
+    contact,
+    title,
+    location,
+  } = job;
+  
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="mt-15">
+      <h1 className="text-center font-bold text-4xl ">Job Details</h1>
+
+      <div className="my-card flex mt-20 justify-center">
+        <div className="jobDeteils w-[840px] ">
+          <p className="py-3">
+            <span className="font-semibold text-xl ">Description: </span>
+            {description}
+          </p>
+          <p className="py-3">
+            {" "}
+            <span className="font-semibold text-xl ">Responsibility: </span>
+            {responsibility}
+          </p>
+          <p className="py-3">
+            {" "}
+            <span className="font-semibold text-xl ">
+              Educational Requirements:{" "}
+            </span>
+            <br />
+            {educational_requirements}
+          </p>
+          <p className="py-3">
+            {" "}
+            <span className="font-semibold text-xl ">
+              Experiences:{" "}
+            </span> <br /> {experiences}
+          </p>
+        </div>
+        <div className="jobInfo  w-96">
+          <div className="information p-5">
+            <div>
+              <p className="font-semibold  text-xl divide-y ">
+                Job Details
+              </p>
+            </div>
+            <hr />
+            <p className="flex mt-2 items-center">
+              <CurrencyDollarIcon className="w-5 text-[#757575]" />
+              <span className="font-semibold text-xl">Salary:</span> &nbsp; {salary}
+            </p>
+            <p className="flex mt-2 items-center">
+              <CalendarDaysIcon className="w-5 text-[#757575]" />
+              <span className="font-semibold text-xl">Job Title:</span> &nbsp; {title}
+            </p>
+            <div>
+              <p className="font-semibold py-5  text-xl divide-y ">
+                Contract information
+              </p>
+            </div>
+            <hr /> 
+            <p className="flex mt-2 items-center">
+              <PhoneIcon className="w-5 text-[#757575]" />
+              <span className="font-semibold text-xl">Phone:</span> &nbsp; {contact.phone}
+            </p>
+            <p className="flex mt-2 items-center">
+              <EnvelopeIcon className="w-5 text-[#757575]" />
+              <span className="font-semibold text-xl">Email:</span> &nbsp; {contact.email}
+            </p>
+            <p className="flex mt-2 items-center">
+              <CurrencyDollarIcon className="w-5 text-[#757575]" />
+              <span className="font-semibold text-xl">Address:</span> &nbsp; {location}
+            </p>
+            
+          </div>
+          <div className=" w-full  mt-3 ">
+            <p className=" w-full flex justify-center items-center text-center btn">Apply Now</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
