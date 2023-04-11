a86cd09a3cc2d5c6de5ea02728afb53fb97c55ad
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-04-11 13:08:53 +0600

    banner section responsive done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,10 +4,10 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="my-container banner ">
-      <div className="banner-conatiner flex justify-between md:flex-row sm:flex-col items-center">
+      <div className="banner-conatiner grid justify-between items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-2  ">
         <div>
-          <h2 className="text-7xl w-auto font-bold">One Step Closer To Your Dream Job</h2>
-          <p className="my-5 text-gray-600">
+          <h2 className=" text-3xl md:text-4xl lg:text-7xl  font-bold">One Step Closer To Your Dream Job</h2>
+          <p className="my-5 w-full text-gray-600">
             Explore thousands of job opportunities with all the information you
             need. Its your future. Come find it. Manage all your job application
             from start to finish.
@@ -15,7 +15,7 @@ const Banner = () => {
           <p><Button gradientDuoTone="purpleToBlue">Get Started</Button></p>
         </div>
         <div className="w-full">
-          <img className="w-screen" src="../../public/banner.png" alt="" />
+          <img className="w-screen" src="https://i.ibb.co/kcgSrvw/banner.png" alt="" />
         </div>
       </div>
     </div>
